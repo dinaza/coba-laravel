d326738ab3cfbbc34e67940c5cffa85d285b460c
Dina Zakiyah <zakiyahdina2@gmail.com>
2023-07-26 17:32:10 +0700

    membuat array manual

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,23 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
+
+    $blog_posts = [
+        [
+            "title" => "Blog 1",
+            "author" => "Bg Satu",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi possimus accusantium quisquam totam. Consequuntur maiores corporis animi, aut voluptas optio quisquam id, magni ea temporibus modi quibusdam ut repellat quas."
+        ],
+    
+        [
+            "title" => "Blog 2",
+            "author" => "Bg Dua",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore qui iure velit suscipit, culpa aliquid odio quia eaque dolor recusandae nulla placeat minus, error, officiis maiores corporis veritatis similique explicabo? Dignissimos quia dolorum assumenda laudantium repellendus cum doloribus numquam officiis aut totam, eius porro, aperiam alias illo illum. Doloremque, inventore?"
+        ],
+    ];
+
     return view('posts', [
-        "title" => "Blog"
+        "title" => "Blog",
+        "posts" => $blog_posts
     ]);
 });
