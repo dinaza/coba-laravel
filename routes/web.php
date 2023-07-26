b22006cbe300ff7bd187238c739d81f3066eaad1
Dina Zakiyah <zakiyahdina2@gmail.com>
2023-07-26 17:17:15 +0700

    templating menggunakan yield, section, extends, dan include

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "D'Blogs ID",
         "email" => "dblogs@gmail.com",
         "image" => "msbing.jpeg"
@@ -26,5 +29,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title" => "Blog"
+    ]);
 });
